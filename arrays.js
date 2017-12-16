40141f854194f452223af6e7891df9e241fcfa1e
Raul Sanchez <rsanchez_99@msn.com>
2017-12-16 04:34:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,11 +4,11 @@
 // var ingredient4 = "butter"
 // var ingredient5 = "tomato"
 // var ingredient6 = "garlic"
-// 
+//
 // var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese',
 //                       'butter', 'tomato', 'garlic']
-// 
-// var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 
+//
+// var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil',
 //                         'basil', 'oregano']
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 console.log(chocolateBars)
