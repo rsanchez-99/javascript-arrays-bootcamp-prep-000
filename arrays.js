c1556399a8a1d1fb11a9241035af176d8f154f1a
Raul Sanchez <rsanchez_99@msn.com>
2017-12-16 05:02:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,11 +10,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-// function addElementToEndOfArray(array, element) {
-//   array = [...array, element]
-//   return array
-// }
-//
+function addElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
+
 // function destructivelyAddElementToEndOfArray(array, element) {
 //   array.push(element)
 //   return array
