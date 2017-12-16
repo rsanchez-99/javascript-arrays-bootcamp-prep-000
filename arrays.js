b1f28ba953cd56f8925df3eaa82f22b66b7d4ee4
Raul Sanchez <rsanchez_99@msn.com>
2017-12-16 05:15:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 
-function removeElementFromBeginningOfArray(array) {
-  array = array.slice(1)
-  return array
-}
+// function removeElementFromBeginningOfArray(array) {
+//   array = array.slice(1)
+//   return array
+// }
